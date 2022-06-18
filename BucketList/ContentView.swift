0f2a1500cfe9f021fffd9ecd8663d14ae105c0a8
Simran Preet Singh <simran5590@gmail.com>
2022-06-18 00:44:38 -0700

    reset

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,42 +6,11 @@
 //
 
 import SwiftUI
-import LocalAuthentication
 
 struct ContentView: View {
   
-  @State private var isUnlocked = false
-  
   var body: some View {
-    VStack {
-      if isUnlocked {
-        Text("Unlocked")
-      } else {
-        Text("Locked")
-      }
-    }
-    .onAppear {
-      authenticate()
-    }    
-  }
-  
-  func authenticate() {
-    let context = LAContext()
-    var error: NSError?
-    
-    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-      let reason = "We need to unlock your data."
-      context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-        if success {
-          // Authenticated successfully
-          isUnlocked = true
-        } else {
-          // Error
-        }
-      }
-    } else {
-      // No Biometrics available or have not enrolled.
-    }
+    Text("Hello")
   }
 }
 
