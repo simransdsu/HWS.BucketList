df0a9e88e5d83ca785fc6b49b9c6a58d1ad5de9a
Simran Preet Singh <simran5590@gmail.com>
2022-06-18 00:44:06 -0700

    Biometrics example

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,16 +6,47 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
-
-    var body: some View {
-       Text("Hello")
+  
+  @State private var isUnlocked = false
+  
+  var body: some View {
+    VStack {
+      if isUnlocked {
+        Text("Unlocked")
+      } else {
+        Text("Locked")
+      }
     }
+    .onAppear {
+      authenticate()
+    }    
+  }
+  
+  func authenticate() {
+    let context = LAContext()
+    var error: NSError?
+    
+    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+      let reason = "We need to unlock your data."
+      context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+        if success {
+          // Authenticated successfully
+          isUnlocked = true
+        } else {
+          // Error
+        }
+      }
+    } else {
+      // No Biometrics available or have not enrolled.
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
